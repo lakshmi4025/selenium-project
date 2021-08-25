d503e4917efa39ccc2111771837b242416377a57
lakshmi <laksmi.pokuru1994@gmail.com>
2021-08-24 23:51:19 -0700

    testing for selenium

---
+++ src/seleniumHCLpackage/seleniumHCLtest.java
@@ -0,0 +1,18 @@
+package seleniumHCLpackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class seleniumHCLtest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\selenium-java-3.141.59\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Hello Google...");
+		driver.get("http://google.com");
+	    driver.manage().window().maximize();
+	    driver.close();
+
+	}
+
+}
